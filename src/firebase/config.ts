c44036ab7c03620674963cea0e15bef92d3ce737
Mihael Nas <nasmihael@gmail.com>
2025-11-09 10:54:41 +0000

    Le notre n'a pas de storageBucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1090015743519:web:77d748bd3d24d46dc5cf2a",
   "apiKey": "AIzaSyCSrFa2F739z3tm9h5Q6xIJLbdnlXFct9g",
   "authDomain": "studio-3474159211-f2088.firebaseapp.com",
+  "storageBucket": "studio-3474159211-f2088.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1090015743519"
 };
